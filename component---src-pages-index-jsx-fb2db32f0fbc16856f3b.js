9d4ad67ce84a8dd57f2ffe30ceed771419988fcc
Code Sandbox <hello@codesandbox.io>
2021-07-20 06:44:52 +0000

    Deployment from Codesandbox.

---
+++ component---src-pages-index-jsx-fb2db32f0fbc16856f3b.js
@@ -0,0 +1,2 @@
+(self.webpackChunkproject_oregano=self.webpackChunkproject_oregano||[]).push([[230],{4199:function(e,n,t){"use strict";t.r(n);var r=t(7294),u=t(5955);n.default=function(){return r.createElement("div",null,r.createElement(u.wp,null))}}}]);
+//# sourceMappingURL=component---src-pages-index-jsx-fb2db32f0fbc16856f3b.js.map
---
+++ component---src-pages-playground-jsx-3cecd8dba4f9045f1e15.js
@@ -0,0 +1,2 @@
+(self.webpackChunkproject_oregano=self.webpackChunkproject_oregano||[]).push([[898],{3104:function(e,n,l){"use strict";l.r(n);var t=l(7294),a=l(5955);n.default=function(){return t.createElement(a.W2,null,t.createElement(a.S$,null,t.createElement(a.nL,null,"Heading 1"),t.createElement(a.XJ,null,"Heading 2"),t.createElement(a.aC,null,"Heading 3"),t.createElement(a.k8,null,"Heading 4"),t.createElement(a.uT,null,"Body"),t.createElement(a.u7,null,"Highlight Body")))}}}]);
+//# sourceMappingURL=component---src-pages-playground-jsx-3cecd8dba4f9045f1e15.js.map
